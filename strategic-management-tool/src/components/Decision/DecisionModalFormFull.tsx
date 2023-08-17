eeb61c5954d8c79ffd03f5530737f895955f3465
freddy0077 <112951482+freddy-cedibet@users.noreply.github.com>
2023-08-17 00:22:15 +0000

    form

--- strategic-management-tool/src/components/Decision/DecisionModalFormFull.tsx
+++ strategic-management-tool/src/components/Decision/DecisionModalFormFull.tsx
@@ -254,183 +254,185 @@ const DecisionModalForm: React.FC<LessonModalFormProps> = ({
   dataLength,
   onClose,
 }) => {
-  const handleSave = () => {
-    const formattedDate1 = `${new Date(date1).getDate()}-${
-      new Date(date1).getMonth() + 1
-    }-${new Date(date1).getFullYear()}`;
+    const handleSave = () => {
+        const formattedDate1 = `${new Date(date1).getDate()}-${
+            new Date(date1).getMonth() + 1
+        }-${new Date(date1).getFullYear()}`;
 
-    const formattedDate2 = `${new Date(date2).getDate()}-${
-      new Date(date2).getMonth() + 1
-    }-${new Date(date2).getFullYear()}`;
+        const formattedDate2 = `${new Date(date2).getDate()}-${
+            new Date(date2).getMonth() + 1
+        }-${new Date(date2).getFullYear()}`;
 
-    addData({
-      No: dataLength + 1,
-      Decision: decision,
-      Date1: formattedDate1,
-      Date2: formattedDate2,
-      Department: department,
-      Topic: topic,
-      Context: context,
-      Forum: forum,
-      Comments: comments,
-      "Next Step": nextStep,
-      "Approved By": approvedBy,
-      "Actioned By": actionedBy,
+        addData({
+            No: dataLength + 1,
+            Decision: decision,
+            Date1: formattedDate1,
+            Date2: formattedDate2,
+            Department: department,
+            Topic: topic,
+            Context: context,
+            Forum: forum,
+            Comments: comments,
+            "Next Step": nextStep,
+            "Approved By": approvedBy,
+            "Actioned By": actionedBy,
 
-      // ... any additional data fields you want to add
-    });
-    setDate1("");
-    setTopic("");
-    setContext("");
-    setForum("");
-    setApprovedBy("");
-    setComments("");
-    setDecision("");
-    setDepartment("");
-    setDate2("");
-    setNextStep("");
-    setActionedBy("");
-    onClose();
-  };
-  return (
-    <form className="w-full h-full">
-      <div className="flex flex-col w-full h-full justify-between space-y-8  overflow-y-auto max-h-[1800px] scrollbar-thin scrollbar-thumb-zinc-200 ">
-        <h1 className="text-primary-500 text-[18px]">Add A Decision</h1>
-        <div className="space-y-4 w-full">
-          <div className="flex w-full h-[80%] space-x-12 items-start justify-between">
-            <div className="flex flex-col w-2/4 h-full space-y-20">
-              <InputField
-                id="email"
-                label="Decision Date"
-                value={date1}
-                onChange={(e) => setDate1(e.target.value)}
-                type="datepicker"
-                required
-                className="w-full m-0"
-              />
-              <InputField
-                id="email"
-                label="Topic"
-                value={topic}
-                onChange={(e) => setTopic(e.target.value)}
-                type="text"
-                placeholder="Enter the topic name"
-                required
-                className="w-full m-0"
-              />
-              <InputField
-                id="email"
-                label="Context"
-                value={context}
-                onChange={(e) => setContext(e.target.value)}
-                type="text"
-                placeholder="Provide the context"
-                required
-                className="w-full m-0"
-              />
-              <InputField
-                id="email"
-                label="Forum"
-                value={forum}
-                onChange={(e) => setForum(e.target.value)}
-                type="textarea"
-                placeholder="Enter your forum details"
-                required
-                className="w-full m-0 h-[240px]  "
-                characterLimit={120}
-              />
-              <InputField
-                id="email"
-                label="Approved By"
-                value={approvedBy}
-                onChange={(e) => setApprovedBy(e.target.value)}
-                type="text"
-                placeholder="Enter the name"
-                required
-                className="w-full m-0"
-              />
-              <InputField
-                id="email"
-                label="Comments"
-                value={comments}
-                onChange={(e) => setComments(e.target.value)}
-                type="textarea"
-                placeholder="Enter the comments"
-                required
-                className="w-full m-0 h-[240px] mb-4 "
-                characterLimit={120}
-              />
+            // ... any additional data fields you want to add
+        });
+        setDate1("");
+        setTopic("");
+        setContext("");
+        setForum("");
+        setApprovedBy("");
+        setComments("");
+        setDecision("");
+        setDepartment("");
+        setDate2("");
+        setNextStep("");
+        setActionedBy("");
+        onClose();
+    };
+    return (
+        <form className="w-full h-full">
+            <div
+                className="flex flex-col w-full h-full justify-between space-y-8  overflow-y-auto max-h-[1800px] scrollbar-thin scrollbar-thumb-zinc-200 ">
+                <h1 className="text-primary-500 text-[18px]">Add A Decision</h1>
+                <div className="space-y-4 w-full">
+                    <div className="flex w-full h-[80%] space-x-12 items-start justify-between">
+                        <div className="flex flex-col w-2/4 h-full space-y-20">
+                            <InputField
+                                id="email"
+                                label="Decision Date"
+                                value={date1}
+                                onChange={(e) => setDate1(e.target.value)}
+                                type="datepicker"
+                                required
+                                className="w-full m-0"
+                            />
+                            <InputField
+                                id="email"
+                                label="Topic"
+                                value={topic}
+                                onChange={(e) => setTopic(e.target.value)}
+                                type="text"
+                                placeholder="Enter the topic name"
+                                required
+                                className="w-full m-0"
+                            />
+                            <InputField
+                                id="email"
+                                label="Context"
+                                value={context}
+                                onChange={(e) => setContext(e.target.value)}
+                                type="text"
+                                placeholder="Provide the context"
+                                required
+                                className="w-full m-0"
+                            />
+                            <InputField
+                                id="email"
+                                label="Forum"
+                                value={forum}
+                                onChange={(e) => setForum(e.target.value)}
+                                type="textarea"
+                                placeholder="Enter your forum details"
+                                required
+                                className="w-full m-0 h-[240px]  "
+                                characterLimit={120}
+                            />
+                            <InputField
+                                id="email"
+                                label="Approved By"
+                                value={approvedBy}
+                                onChange={(e) => setApprovedBy(e.target.value)}
+                                type="text"
+                                placeholder="Enter the name"
+                                required
+                                className="w-full m-0"
+                            />
+                            <InputField
+                                id="email"
+                                label="Comments"
+                                value={comments}
+                                onChange={(e) => setComments(e.target.value)}
+                                type="textarea"
+                                placeholder="Enter the comments"
+                                required
+                                className="w-full m-0 h-[240px] mb-4 "
+                                characterLimit={120}
+                            />
+                        </div>
+                        <div className="flex flex-col w-2/4  space-y-20 mt-6">
+                            <InputField
+                                id="email"
+                                label="Decision"
+                                value={decision}
+                                onChange={(e) => setDecision(e.target.value)}
+                                type="textarea"
+                                placeholder="Describe the decision"
+                                required
+                                className="w-full m-0 h-[320px] mb-4 "
+                                characterLimit={120}
+                            />
+                            <InputField
+                                id="email"
+                                label="Department"
+                                value={department}
+                                onChange={(e) => setDepartment(e.target.value)}
+                                type="text"
+                                placeholder="Enter the department name"
+                                required
+                                className="w-full m-0"
+                            />
+                            <InputField
+                                id="email"
+                                label="Date2"
+                                value={date2}
+                                onChange={(e) => setDate2(e.target.value)}
+                                type="datepicker"
+                                required
+                                className="w-full m-0"
+                            />
+                            <InputField
+                                id="email"
+                                label="Next Step"
+                                value={nextStep}
+                                onChange={(e) => setNextStep(e.target.value)}
+                                type="text"
+                                placeholder="Describe the next step"
+                                required
+                                className="w-full m-0"
+                                characterLimit={120}
+                            />
+                            <InputField
+                                id="email"
+                                label="Actioned By"
+                                value={actionedBy}
+                                onChange={(e) => setActionedBy(e.target.value)}
+                                type="text"
+                                placeholder="Enter the name"
+                                required
+                                className="w-full m-0"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex w-full h-[20%] items-end justify-end ">
+                        <Button
+                            variant="primary"
+                            size="lg"
+                            onClick={handleSave}
+                            className="rounded-lg w-[30%] mt-6 bg-primary-500"
+                            type="button"
+                        >
+                            Add A Decision
+                        </Button>
+                    </div>
+                </div>
             </div>
-            <div className="flex flex-col w-2/4  space-y-20 mt-6">
-              <InputField
-                id="email"
-                label="Decision"
-                value={decision}
-                onChange={(e) => setDecision(e.target.value)}
-                type="textarea"
-                placeholder="Describe the decision"
-                required
-                className="w-full m-0 h-[320px] mb-4 "
-                characterLimit={120}
-              />
-              <InputField
-                id="email"
-                label="Department"
-                value={department}
-                onChange={(e) => setDepartment(e.target.value)}
-                type="text"
-                placeholder="Enter the department name"
-                required
-                className="w-full m-0"
-              />
-              <InputField
-                id="email"
-                label="Date2"
-                value={date2}
-                onChange={(e) => setDate2(e.target.value)}
-                type="datepicker"
-                required
-                className="w-full m-0"
-              />
-              <InputField
-                id="email"
-                label="Next Step"
-                value={nextStep}
-                onChange={(e) => setNextStep(e.target.value)}
-                type="text"
-                placeholder="Describe the next step"
-                required
-                className="w-full m-0"
-                characterLimit={120}
-              />
-              <InputField
-                id="email"
-                label="Actioned By"
-                value={actionedBy}
-                onChange={(e) => setActionedBy(e.target.value)}
-                type="text"
-                placeholder="Enter the name"
-                required
-                className="w-full m-0"
-              />
-            </div>
-          </div>
-          <div className="flex w-full h-[20%] items-end justify-end ">
-            <Button
-              variant="primary"
-              size="lg"
-              onClick={handleSave}
-              className="rounded-lg w-[30%] mt-6 bg-primary-500"
-              type="button"
-            >
-              Add A Decision
-            </Button>
-          </div>
-        </div>
-      </div>
-    </form>
-  );
+        </form>
+    );
 
+}
 };
 
 export default DecisionModalForm;
